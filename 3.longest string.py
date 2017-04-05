ef28976a75a39a0fef2bccb0f083ba1fb7e33f27
quaner2557 <qq974807643@gmail.com>
2017-04-05 18:40:04 +0800

    forget lis[s[i]]+1

--- 3.longest string.py
+++ 3.longest string.py
@@ -9,7 +9,7 @@ class Solution(object):
 
         for i in range(len(s)):
             if s[i] in lis and start <= lis[s[i]] :
-                start = lis[s[i]]
+                start = lis[s[i]]+1
             else:
                 n = max(n,i-start+1)
 
